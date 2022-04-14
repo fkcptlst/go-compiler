7804de8ae9c27308ffa835d836861fc1cfb8fd4b
程荣 <chengrong@tju.edu.cn>
2022-04-14 21:00:24 +0800

    1. create TAC 2. add struct SymbolTabel 3. add class SymbolManager

--- include/Common.h
+++ include/Common.h
@@ -2,6 +2,13 @@
 #define INCLUDE_COMMON_H_
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <cassert>
 
 
 #endif // INCLUDE_COMMON_H_
