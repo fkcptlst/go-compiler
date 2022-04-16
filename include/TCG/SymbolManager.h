bc01acf5af8772b6b1b78fbf93eda813e094b6f0
程荣 <chengrong@tju.edu.cn>
2022-04-16 13:44:24 +0800

    feat(TCG): 添加 get_reg 接口

--- include/TCG/SymbolManager.h
+++ include/TCG/SymbolManager.h
@@ -7,31 +7,44 @@
 #include "TCG/ASM.h"
 
 
-/* crTODO */
-#define reg_nums 10
-
-
 class SymbolManager {
 public:
 	/* 返回寄存器 reg 的变量名, 无意义则返回 空串 */
 	std::string rvalue(REG reg);
 
 	/* 返回该变量的地址 内存地址, 寄存器地址(0 ~ regnums) */
-	uint64_t avalue(const std::string& vairable);
+	REG avalue_reg(const std::string& vairable);
+	uint64_t avalue_mem(const std::string& vairable);
+	void set_variable(const std::string& variable, REG reg=REG::None, uint64_t mem=0);
 
-	/* 给变量 vairable 分配一个寄存器 */
-	REG get_reg(const std::string& vairable);
+	/* 给 dst 变量分配一个寄存器 */
+	REG get_reg(const TACLine& line);
 
 	/* 计算一个代码块中, 变量的待用信息和活跃信息 */
 	void cal_use_info(TACBlock& block, SymbolTabel& symbol_tabel);
 
 private:
+	/* 获得一个空间寄存器 */
+	REG get_free_reg();
+
+private:
 	/* 寄存器描述数组 */
-	std::string rvalue_[reg_nums];
+	std::string rvalue_[static_cast<int>(REG::None)];
 
 	/* 变量地址描述数组 */
-	std::unordered_map<std::string, uint64_t> avalue_;
+	std::unordered_map<std::string, REG> avalue_reg_;
+	std::unordered_map<std::string, uint64_t> avalue_mem_;
 };
+
+
+inline REG SymbolManager::get_free_reg() {
+	for (int i(0); i < static_cast<int>(REG::None); i++) {
+		if (rvalue_[i] != "") {
+			return static_cast<REG>(i);
+		}
+	}
+	return REG::None;
+}
 
 
 inline std::string SymbolManager::rvalue(REG reg) {
@@ -39,8 +52,32 @@ inline std::string SymbolManager::rvalue(REG reg) {
 }
 
 
-inline uint64_t SymbolManager::avalue(const std::string& vairable) {
-	return avalue_[vairable];
+inline REG SymbolManager::avalue_reg(const std::string& vairable) {
+	if (avalue_reg_.find(vairable) != avalue_reg_.end()) {
+		return avalue_reg_[vairable];
+	} else {
+		return REG::None;
+	}
+}
+
+
+inline uint64_t SymbolManager::avalue_mem(const std::string& vairable) {
+	if (avalue_mem_.find(vairable) != avalue_mem_.end()) {
+		return avalue_mem_[vairable];
+	} else {
+		return 0;
+	}
+}
+
+
+inline void SymbolManager::set_variable(const std::string& variable, REG reg, uint64_t mem) {
+	if (mem != 0) {
+		avalue_mem_[variable] = mem;
+	}
+	if (reg != REG::None) {
+		avalue_reg_[variable] = reg;
+		rvalue_[static_cast<int>(reg)] = variable;
+	}
 }
 
 
--- src/TCG/SymbolManager.cpp
+++ src/TCG/SymbolManager.cpp
@@ -1,8 +1,23 @@
 #include "TCG/SymbolManager.h"
 
 
-REG SymbolManager::get_reg(const std::string& vairable) {
-	/* crTODO */
+REG SymbolManager::get_reg(const TACLine& line) {
+	/* crTODO: 在 translator 中, 也存在修改 两个数组的情况!!! */
+	/* crTODO: 目前默认认为如果变量移除寄存器, 可以保证该变量以后不会使用 或 该变量内存中有位置 */
+	if (avalue_reg(line.src1.value) != REG::None) {
+		/* 使用 src1 的 寄存器 */
+		avalue_reg_[line.dst.value] = avalue_reg(line.src1.value);
+		return avalue_reg(line.dst.value);
+	}
+
+	/* 用空闲寄存器 */
+	REG reg = get_free_reg();
+	if (reg != REG::None) {
+		avalue_reg_[line.dst.value] = reg;
+		return reg;
+	}
+
+	/* crTODO: 替换寄存器 */
 	return REG::None;
 }
 
@@ -11,7 +26,7 @@ void SymbolManager::cal_use_info(TACBlock& block, SymbolTabel& symbol_tabel) {
 	/* crTODO: 基本块之后???? */
 
 	/* 使用vector逆向迭代器 从后向前扫描三地址码 */
-	for (auto it = block.rbegin(); it != block.rend(); ++it) {
+	for (auto it = block.rbegin(); it != block.rend(); it++) {
 		TACLine &line = *it;
 		/* 把符号表中 dst 的待用信息和活跃信息 附加到 中间代码上 */
 		line.dst.use_info = symbol_tabel[line.dst.value].use_info;
