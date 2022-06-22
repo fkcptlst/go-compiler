88169c02b0308b591745ed57818908c3e75433f5
singularity <lijinghan@tju.edu.cn>
2022-06-22 16:52:18 +0800

    createlist translator

--- src/TCG/SentenceTranslator/CreatelistTranslator.cpp
+++ src/TCG/SentenceTranslator/CreatelistTranslator.cpp
@@ -7,6 +7,15 @@
 
 ASMLines CreatelistTranslator::SentenceTranslate_(SymbolManager& SymbolManager_, TACLine& TACLine_) {
     ASMLines asmlines;
-    
+    std::string array_str_encode = SymbolManager_.encode_var(TACLine_.src1.value);
+    int array_space = 4 * std::stoi(TACLine_.src2.value);
+    SymbolManager_.set_esp_bias(4);
+    asmlines.push_back(construct_asm("sub", REG::ESP, std::to_string(4)));
+    int array_ptr_mem = SymbolManager_.get_esp() - 4;
+    SymbolManager_.set_avalue_mem(array_str_encode, array_ptr_mem);
+    SymbolManager_.set_esp_bias(array_space);
+    asmlines.push_back(construct_asm("sub", REG::ESP, std::to_string(array_space)));
+    asmlines.push_back(construct_asm("mov", array_ptr_mem, REG::ESP));
+
     return asmlines;
 }
