7a76558f4d3008e9fb756f2a717597fbdfb272a6
3019209351-Liu Xueyang <3019209351@tju.edu.cn>
2022-06-30 20:17:26 +0800

    change

--- scripts/run_asm.sh
+++ scripts/run_asm.sh
@@ -1,5 +1,5 @@
-nasm -f elf32 -o ${1}.o ../output/${1}.asm
-nasm -f elf32 -o print.o ../example/print.asm
+# nasm -f elf32 -o ${1}.o ../output/${1}.asm
+# nasm -f elf32 -o print.o ../example/print.asm
 ld -m elf_i386 -o run.bin ${1}.o print.o
 ./run.bin
 
