3e4450816ca9927a25bb78958e57665983b03c0b
chengrong <chengrong@tju.edu.cn>
2022-07-01 23:29:51 +0800

    test(demo): 更新测试文件

---
+++ demo/02calculate.go
@@ -0,0 +1,11 @@
+package main
+
+
+func myprint(a int)
+
+
+func main() {
+	var a int = 4 - (2 + 4 - 4 * 2) + 3
+	a++
+	myprint(a)					// 预计输出 10
+}
--- demo/04for.go
+++ demo/04for.go
@@ -5,12 +5,10 @@ func myprint(a int)
 
 
 func main() {
-	var sum = 0
 	var i int
+	var sum = 0
 	for i = 0; i <= 10; i++ {
 		sum += i
-		myprint(i + i)
 		myprint(sum)
 	}
-	myprint(sum)
 }
