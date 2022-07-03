436a861fda0aab8c1de7d9a241aaa1446b5ed000
chengrong <chengrong@tju.edu.cn>
2022-07-03 22:23:32 +0800

    scripts(run_asm)

--- scripts/run_asm.sh
+++ scripts/run_asm.sh
@@ -1,5 +1,5 @@
-nasm -f elf32 -o out.o ../output/out.asm
-nasm -f elf32 -o print.o ../demo/print.asm
+# nasm -f elf32 -o out.o ../output/out.asm
+# nasm -f elf32 -o print.o ../demo/print.asm
 ld -m elf_i386 -o run.bin out.o print.o
 ./run.bin
 
