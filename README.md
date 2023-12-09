ab7f7b76c219c5e6cca85f5a4e3c4692634b5a41
lcf <kevinfinley@163.com>
2023-12-09 22:48:39 +0800

    added unit test utils

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # go-compiler
+
+## setup project
+
+```bash
+pip install -e .[dev]
+```
+
+## run tests (with coverage)
+
+```bash
+pytest --cov=go_compiler tests/
+```
---
+++ tests/test_sample.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+
+def test_answer():
+    assert add(3, 4) == 7
+
+
+from go_compiler.main import add2
+
+
+
+def test_answer2():
+    assert add2(3, 4) == 7
