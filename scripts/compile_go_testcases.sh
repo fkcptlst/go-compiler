792cf7ef4ad5de9f972faf0aa798142180ca8faf
lcf <kevinfinley@163.com>
2023-12-29 00:30:28 +0800

    added compile script for go

---
+++ scripts/compile_go_testcases.sh
@@ -0,0 +1,24 @@
+go_source_dir=$1
+output_dir=./tmp
+
+# 1. setup output dir
+if [ ! -d $output_dir ]; then
+  mkdir -p $output_dir
+fi
+
+# 2. replace my_print with fmt.Print
+for file in "$go_source_dir"/*.go; do
+    if [ -f "$file" ]; then
+        output_file="$output_dir/$(basename "$file")"
+        # replace myprint with fmt.Print
+        sed '/func myprint(a int)/c\
+import "fmt"\
+func myprint(a int) {\
+    fmt.Print(a)\
+}' "$file" > "$output_file"
+
+    fi
+done
+
+# 3. compile
+cd $output_dir && find . -name "*.go" | xargs -I {} go build {}
